3b645c0686fdde5fd90095f434a18955bf24dc78
Anes Belfodil <ans.belfodil@gmail.com>
2022-06-23 21:18:46 +0000

    Generalize round impl

--- src/num.rs
+++ src/num.rs
@@ -1,9 +1,11 @@
+use num::Float;
+
 pub trait Round {
     fn signed_ceil(&self) -> Self;
     fn signed_floor(&self) -> Self;
 }
 
-impl Round for f64 {
+impl<T: Float> Round for T {
     fn signed_ceil(&self) -> Self {
         self.signum() * self.abs().ceil()
     }
