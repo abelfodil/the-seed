ccd6d682fda5fe26694ce8b6f1343f7512e5cd74
Anes Belfodil <ans.belfodil@gmail.com>
2022-08-01 02:09:58 +0000

    Use default feature and add path element

--- src/element.rs
+++ src/element.rs
@@ -4,20 +4,23 @@ use rand::Rng;
 pub trait WorldElement: Default + Into<Pixel> + Copy + RandomValue {}
 impl<T> WorldElement for T where T: Default + Into<Pixel> + Copy + RandomValue {}
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Default)]
 pub enum BasicElement {
     Terrain,
     Wall,
+    Path,
+    #[default]
     Void,
 }
 
 impl RandomValue for BasicElement {
     fn random<R: Rng + ?Sized>(rng: &mut R) -> BasicElement {
-        let index: u8 = rng.gen_range(0..3);
+        let index: u8 = rng.gen_range(0..4);
         match index {
             0 => BasicElement::Terrain,
             1 => BasicElement::Wall,
-            2 => BasicElement::Void,
+            2 => BasicElement::Path,
+            3 => BasicElement::Void,
             _ => unreachable!(),
         }
     }
@@ -30,13 +33,8 @@ impl Into<Pixel> for BasicElement {
         match self {
             BasicElement::Terrain => [0, 255, 0, 255],
             BasicElement::Wall => [0, 0, 255, 255],
+            BasicElement::Path => [255, 0, 0, 0],
             BasicElement::Void => [0, 0, 0, 0],
         }
-    }
-}
-
-impl Default for BasicElement {
-    fn default() -> BasicElement {
-        BasicElement::Void
     }
 }
